4938ea0cf63500a45969d42a7effa3ca739973c0
Frithjof Petrick <fmpetrick@gmail.com>
2021-07-29 19:31:50 +0200

    opengl.slp: Matrix, uniforms

--- usage_examples/shaders/vertex.glsl
+++ usage_examples/shaders/vertex.glsl
@@ -1,7 +1,8 @@
 #version 330 core
 layout(location = 0) in vec3 vertexPosition_modelspace;
 
+uniform mat4 projection;
+
 void main() {
-  gl_Position.xyz = vertexPosition_modelspace;
-  gl_Position.w = 1.0;
+  gl_Position = projection * vec4(vertexPosition_modelspace, 1);
 }
