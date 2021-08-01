832de2479f46089505bd9935c140062e56349a5e
Leander Behr <l.behr19199@gmail.com>
2021-08-01 15:21:28 +0200

    Add tests for using expressions as statements

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,7 +7,14 @@ from tests.run_examples_dir import make_example_cases
 
 
 def test_examples():
-   for x in make_example_cases('examples'): yield x
+  for x in make_example_cases('examples'):
+    yield x
+
+
+def test_exprs_as_stmts():
+  for x in make_example_cases('examples_exprs_as_stmts'):
+    yield x
+
 
 if __name__ == "__main__":
   try:
