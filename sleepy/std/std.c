e5eeec4c8d28ee71621f4e70ee7d5c3777454007
Frithjof Petrick <fmpetrick@gmail.com>
2021-09-02 17:56:07 +0200

    Cleanup std.c

--- sleepy/std/std.c
+++ sleepy/std/std.c
@@ -46,85 +46,8 @@ long* allocate_long(int size) {
     return malloc((size_t) size * sizeof(long));
 }
 
-char* double_null_ptr() {
-    return NULL;
-}
-float* float_null_ptr() {
-    return NULL;
-}
-char* char_null_ptr() {
-    return NULL;
-}
-int* int_null_ptr() {
-    return NULL;
-}
-long* long_null_ptr() {
-    return NULL;
-}
-
-
 void deallocate_double(double* ptr) {
     free(ptr);
-}
-void deallocate_float(float* ptr) {
-    free(ptr);
-}
-void deallocate_char(char* ptr) {
-    free(ptr);
-}
-void deallocate_int(int* ptr) {
-    free(ptr);
-}
-void deallocate_long(long* ptr) {
-    free(ptr);
-}
-
-void store_double(double* ptr, double value) {
-    *ptr = value;
-}
-void store_float(float* ptr, float value) {
-    *ptr = value;
-}
-void store_char(char* ptr, char value) {
-    *ptr = value;
-}
-void store_int(int* ptr, int value) {
-    *ptr = value;
-}
-void store_long(long* ptr, long value) {
-    *ptr = value;
-}
-
-double load_double(double* ptr) {
-    return *ptr;
-}
-float load_float(float* ptr) {
-    return *ptr;
-}
-char load_char(char* ptr) {
-    return *ptr;
-}
-int load_int(int* ptr) {
-    return *ptr;
-}
-long load_long(long* ptr) {
-    return *ptr;
-}
-
-void memcpy_double(double* to, double* from, int len) {
-    memcpy(to, from, len);
-}
-void memcpy_float(float* to, float* from, int len) {
-    memcpy(to, from, len);
-}
-void memcpy_char(char* to, char* from, int len) {
-    memcpy(to, from, len);
-}
-void memcpy_int(int* to, int* from, int len) {
-    memcpy(to, from, len);
-}
-void memcpy_long(long* to, long* from, int len) {
-    memcpy(to, from, len);
 }
 
 int double_to_int(double d) {
