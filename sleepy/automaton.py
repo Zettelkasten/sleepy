9615b9a660ecec2e853802e01749c6ed3f0a6fd4
leanderBehr <leandersbehr@gmail.com>
2021-10-02 16:35:56 +0200

    Clean up automaton.py

--- sleepy/automaton.py
+++ sleepy/automaton.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Set, FrozenSet
+from typing import List, Dict, Set, FrozenSet, Optional, Iterator, Iterable
 
 from sleepy.grammar import EPSILON
 
@@ -13,29 +13,17 @@ class NonDeterministicAutomaton:
   A NFA with epsilon transitions.
   """
 
-  def __init__(self, initial_state, final_states, state_transition_table):
-    """
-    :param int initial_state:
-    :param set[int] final_states:
-    :param list[dict[str, set[int]]] state_transition_table:
-    """
+  def __init__(self, initial_state: int, final_states: Set[int], state_transition_table: List[Dict[str, Set[int]]]):
     self.initial_state = initial_state
     self.final_states = final_states
     self.state_transition_table = state_transition_table
 
-  def get_next_states(self, state, char):
-    """
-    :param int state:
-    :param str|None char: char or epsilon
-    :rtype: set[str]
-    """
+  def get_next_states(self, state: int, char: Optional[str]) -> Set[str]:
     return self.state_transition_table[state].get(char, set())
 
-  def get_epsilon_closure(self, states):
+  def get_epsilon_closure(self, states: Iterable[int]) -> Set[int]:
     """
     Gets all states reachable from `states` only using epsilon transitions
-    :param set[int]|list[int] states:
-    :rtype: set[int]
     """
     state_set = set()
     to_add = list(states)
@@ -48,11 +36,7 @@ class NonDeterministicAutomaton:
       to_add.extend(self.get_next_states(state, EPSILON))
     return state_set
 
-  def accepts(self, word):
-    """
-    :param str word:
-    :rtype: bool
-    """
+  def accepts(self, word: str) -> bool:
     state_set = self.get_epsilon_closure({self.initial_state})
     for char in word:
       state_set = self.get_epsilon_closure({s for state in state_set for s in self.get_next_states(state, char)})
@@ -64,42 +48,28 @@ class DeterministicAutomaton:
   A DFA (without epsilon-transitions).
   """
 
-  def __init__(self, initial_state, final_states, state_transition_table):
-    """
-    :param int initial_state:
-    :param set[int] final_states:
-    :param list[dict[str, int]] state_transition_table:
-    """
+  def __init__(self, initial_state: int, final_states: Set[int], state_transition_table: List[Dict[str, int]]):
     self.initial_state = initial_state
     self.final_states = final_states
     self.state_transition_table = state_transition_table
 
-  def get_next_state(self, state, char):
+  def get_next_state(self, state: Optional[int], char: Optional[str]) -> Optional[str]:
     """
-    :param int|None state:
-    :param str|None char: char
     :returns: next state or `ERROR_STATE` if next state is not productive
-    :rtype: str|None
     """
     if state is ERROR_STATE:
       return ERROR_STATE
     return self.state_transition_table[state].get(char, ERROR_STATE)
 
-  def get_next_possible_chars(self, state):
+  def get_next_possible_chars(self, state: Optional[int]) -> Set[str]:
     """
-    :param int|None state:
     :returns: all characters that transition to a productive state
-    :rtype: set[str]
     """
     if state is ERROR_STATE:
       return set()
     return set(self.state_transition_table[state].keys())
 
-  def accepts(self, word):
-    """
-    :param str word:
-    :rtype: bool
-    """
+  def accepts(self, word: str) -> bool:
     state = self.initial_state
     for char in word:
       if char not in self.state_transition_table[state]:
@@ -108,21 +78,16 @@ class DeterministicAutomaton:
     return state in self.final_states
 
 
-def make_dfa_from_nfa(nfa):
+def make_dfa_from_nfa(nfa: NonDeterministicAutomaton) -> DeterministicAutomaton:
   """
   Applies powerset construction.
-  :param NonDeterministicAutomaton nfa:
-  :rtype: DeterministicAutomaton
   """
   powerset_idx_transition_table: List[Dict[str, int]] = []
   final_powersets_idx: Set[int] = set()
   added_powersets: Dict[FrozenSet[int], int] = {}
   dirty_powersets: Set[FrozenSet[int]] = set()
 
-  def add_next_powersets(powerset):
-    """
-    :param frozenset[int] powerset:
-    """
+  def add_next_powersets(powerset: FrozenSet[int]):
     assert powerset in added_powersets
     powerset_idx = added_powersets[powerset]
     added_chars: Set[str] = set()
