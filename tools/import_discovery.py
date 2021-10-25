1bffee0e9b19c3e33f3c5c9339d86901392ca146
Frithjof Petrick <fmpetrick@gmail.com>
2021-10-26 01:10:30 +0200

    Imports: Add std lib to search path

--- tools/import_discovery.py
+++ tools/import_discovery.py
@@ -1,5 +1,6 @@
+import os.path
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 import networkx as nx
 
@@ -18,18 +19,29 @@ def make_ast_for_file(path: Path) -> FileAst:
 
 def resolve_import_path(import_path: Path, importing_path: Path) -> Path:
   if import_path.is_absolute():
-    return importing_path.resolve()
+    return import_path.resolve()
   return importing_path.parent.joinpath(import_path).resolve()
 
 
 def process_file(path: Path, dag: nx.DiGraph) -> List[Path]:
+  from sleepy.jit import LIBRARY_PATH
+  search_paths = ['', LIBRARY_PATH]
+
   current_ast = make_ast_for_file(path)
   dag.nodes[path]["file_ast"] = current_ast
 
   new_nodes = []
 
   for child in current_ast.imports_ast.imports:
-    child = resolve_import_path(import_path=Path(child), importing_path=path)
+    child_path: Optional[Path] = None
+    for search_path in search_paths:
+      check_path = Path(search_path, child)
+      if os.path.exists(check_path):
+        child_path = check_path
+        break
+    if child_path is None:
+      current_ast.imports_ast.raise_error('Cannot resolve import %r.\nSarched in %r' % (child, search_paths))
+    child = resolve_import_path(import_path=child_path, importing_path=path)
 
     if child not in dag.nodes:
       dag.add_node(child, file_ast=None)
