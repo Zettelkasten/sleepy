74fe9cc1f1e9cfd98d19d5a1ae9b96466b66ec5d
Frithjof Petrick <fmpetrick@gmail.com>
2021-12-29 22:29:34 +0100

    Support special ASCII chars
    Fix #72

--- sleepy/syntactical_analysis/regex.py
+++ sleepy/syntactical_analysis/regex.py
@@ -11,7 +11,7 @@ from sleepy.syntactical_analysis.parser import ParserGenerator
 REGEX_LIT_TOKEN = 'a'
 REGEX_SPECIAL_TOKENS = frozenset({'(', ')', '\\', '-', '[', ']', '*', '+', '?', '|', '^', '.'})
 # Currently we only recognize 7-bit ASCII
-REGEX_RECOGNIZED_CHARS = frozenset({'\n', OTHER_CHAR} | {chr(c) for c in range(32, 128)})
+REGEX_RECOGNIZED_CHARS = frozenset({OTHER_CHAR} | {chr(c) for c in range(0, 128)})
 
 REGEX_CHOICE_OP = Production('Choice', 'Choice', '|', 'Concat')
 REGEX_CONCAT_OP = Production('Concat', 'Concat', 'Repeat')
