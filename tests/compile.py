03c8c0f0aa3b1462d3eb3428cb758abfc7767b09
Frithjof Petrick <fmpetrick@gmail.com>
2022-02-07 18:54:06 +0100

    more

--- tests/compile.py
+++ tests/compile.py
@@ -5,7 +5,6 @@ from llvmlite.binding import ExecutionEngine
 from sleepy.syntactical_analysis.grammar import DummyPath
 from sleepy.jit import compile_ir
 from sleepy.ast_generation import make_translation_unit_ast_from_str
-from sleepy.types import OverloadSet
 
 
 def compile_program(engine: ExecutionEngine,
@@ -15,14 +14,15 @@ def compile_program(engine: ExecutionEngine,
                     emit_debug=True) -> Callable:
   file_path = DummyPath("test")
   ast = make_translation_unit_ast_from_str(file_path=file_path, program=program, add_preamble=add_preamble)
-  module_ir, symbol_table = ast.make_module_ir_and_symbol_table(
+  module_ir, symbol_table, exported_funcs = ast.make_module_ir_and_symbol_table(
     module_name='test_parse_ast', emit_debug=emit_debug, implicitly_exported_functions={main_func_identifier})
   print('---- module intermediate repr:')
   print(module_ir)
   compile_ir(engine, module_ir)
-  assert main_func_identifier in symbol_table
-  main_func_symbol = symbol_table[main_func_identifier]
-  assert isinstance(main_func_symbol, OverloadSet)
-  py_func = main_func_symbol.get_single_concrete_func().make_py_func(engine)
+
+  main_func = next((func for func in exported_funcs if func.indentifier == main_func_identifier), None)
+  assert main_func is not None
+  py_func = main_func.make_py_func(engine)
+
   assert callable(py_func)
   return py_func
